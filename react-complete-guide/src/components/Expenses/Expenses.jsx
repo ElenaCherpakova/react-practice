0b6fea4be52a41b3c383825841a80492c88011ae
ElenaCherpakova <elena.cherpakova@gmail.com>
2022-03-09 21:06:43 -0500

    create components and pass props

---
+++ react-complete-guide/src/components/Expenses/Expenses.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
+import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+
+export default function Expenses(props) {
+  const [filteredYear, setFilteredYear] = useState("2022");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  const { items } = props;
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        <ExpenseItem
+          title={items[0].title}
+          amount={items[0].amount}
+          date={items[0].date}
+        />
+        <ExpenseItem
+          title={items[1].title}
+          amount={items[1].amount}
+          date={items[1].date}
+        />
+        <ExpenseItem
+          title={items[2].title}
+          amount={items[2].amount}
+          date={items[2].date}
+        />
+        <ExpenseItem
+          title={items[3].title}
+          amount={items[3].amount}
+          date={items[3].date}
+        />
+      </Card>
+    </div>
+  );
+}
