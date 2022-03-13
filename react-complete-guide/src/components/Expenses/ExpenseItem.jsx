a9575106b096d9e086a6b8aef1488958156529c8
ElenaCherpakova <elena.cherpakova@gmail.com>
2022-03-13 18:04:03 -0400

    add condition if expenses arenot found

--- react-complete-guide/src/components/Expenses/ExpenseItem.jsx
+++ react-complete-guide/src/components/Expenses/ExpenseItem.jsx
@@ -5,6 +5,7 @@ import Card from "../UI/Card";
 
 export default function ExpenseItem(props) {
   return (
+    <li>
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
@@ -12,5 +13,6 @@ export default function ExpenseItem(props) {
         <div className="expense-item__price">$ {props.amount}</div>
       </div>
     </Card>
+    </li>
   );
 }
